cb797ca74e703e1d9965780d481e8b113d4479b5
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-10-15 14:32:44 +0300

    get data by form (id/name)
    added EL,axios get request,append data func.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 
+const appendPokemonData = async ()=>{
+    const pokeRelevantData = await getPokemonData();
+    //append data
+    document.getElementById("poke-name").innerText = pokeRelevantData.name ;
+    document.getElementById("height").innerText = pokeRelevantData.height ;
+    document.getElementById("weight").innerText = pokeRelevantData.weight ;
+    document.getElementById("poke-img").src = pokeRelevantData.image ;
+}
+
+const getPokemonData = async ()=>{
+        const pokeId = document.getElementById('search-inpt').value;
+        const ApiResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`)
+        const pokeData = ApiResult.data;
+        return { name : pokeData.species.name,
+                 height : pokeData.height, 
+                 weight : pokeData.weight,
+                 image : pokeData.sprites.front_default,
+                 backImage : pokeData.sprites.back_default
+               }
+}
+
+document.getElementById('search-btn').addEventListener('click',appendPokemonData);
