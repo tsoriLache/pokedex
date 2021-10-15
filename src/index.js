1ba38827bf6772e1c5d7a786c8b74f7e4400517d
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-10-15 17:33:52 +0300

    moved try&catch to the seconed function
    i moved it because i saw it resume the func after the error and create another error by trying to use undifined

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-
 const appendPokemonData = async ()=>{
-    const pokeRelevantData = await getPokemonData();
-    //append data
-    document.getElementById("poke-name").innerText = pokeRelevantData.name ;
-    document.getElementById("height").innerText = pokeRelevantData.height ;
-    document.getElementById("weight").innerText = pokeRelevantData.weight ;
-    document.getElementById("poke-img").src = pokeRelevantData.image ;
+    try{
+        const pokeRelevantData = await getPokemonData();
+        //append data
+        document.getElementById("poke-name").innerText = pokeRelevantData.name ;
+        document.getElementById("height").innerText = pokeRelevantData.height ;
+        document.getElementById("weight").innerText = pokeRelevantData.weight ;
+        document.getElementById("poke-img").src = pokeRelevantData.image ;
+        typeList = pokeRelevantData.types.map((type)=>type.type.name);
+        appendTypeList(typeList);
+        return pokeRelevantData; //     added for display..need to find better solution.
+    }
+    catch{
+        document.getElementById('not-found-msg').style='display: block';
+    }
+}
+
+const appendTypeList = (list)=>{
+    for(li of list){
+        createTypeLiElement(li);
+    }
+}
+
+const createTypeLiElement = (li)=>{
+    const btn = document.createElement('button');
+    btn.setAttribute("class", "list-group-item list-group-item-action");  
+    btn.setAttribute("type", "button");  
+    btn.innerText = li;         
+    btn.addEventListener('click',getPokémonsByType)
+    document.getElementById('types-list').appendChild(btn); 
 }
 
 const getPokemonData = async ()=>{
-    try{
         const pokeId = document.getElementById('search-inpt').value;
         const ApiResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`)
         const pokeData = ApiResult.data;
@@ -17,13 +38,11 @@ const getPokemonData = async ()=>{
                  height : pokeData.height, 
                  weight : pokeData.weight,
                  image : pokeData.sprites.front_default,
-                 backImage : pokeData.sprites.back_default
-               }
-    }
-    catch{
-        document.getElementById('not-found-msg').style='display: block'
-    }
+                 backImage : pokeData.sprites.back_default,
+                 types : pokeData.types
+                }
 }
+
 
 const changeToBackImg = async ()=>{
     const pokeRelevantData = await getPokemonData();
