9e40734b755609b648d516a77965f8395c37321e
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-10-15 14:34:57 +0300

    added try&catch and error msg
    EL,msg from bootstrap,

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const appendPokemonData = async ()=>{
 }
 
 const getPokemonData = async ()=>{
+    try{
         const pokeId = document.getElementById('search-inpt').value;
         const ApiResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`)
         const pokeData = ApiResult.data;
@@ -18,6 +19,12 @@ const getPokemonData = async ()=>{
                  image : pokeData.sprites.front_default,
                  backImage : pokeData.sprites.back_default
                }
+    }
+    catch{
+        document.getElementById('not-found-msg').style='display: block'
+    }
+}
+
 const changeToBackImg = async ()=>{
     const pokeRelevantData = await getPokemonData();
     document.getElementById("poke-img").src = pokeRelevantData.backImage ;
@@ -28,8 +35,11 @@ const changeToFrontImg = async ()=>{
     document.getElementById("poke-img").src = pokeRelevantData.image;
 }
 
+const closeNotFoundMsg = ()=>{
+    document.getElementById('not-found-msg').style='display: none'
 }
 
 document.getElementById('search-btn').addEventListener('click',appendPokemonData);
 document.getElementById('poke-img').addEventListener('mouseover',changeToBackImg);
 document.getElementById('poke-img').addEventListener('mouseout',changeToFrontImg);
+document.getElementById('close-msg').addEventListener('click',closeNotFoundMsg);
