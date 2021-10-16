a9f7706c0d3f1dbd360e02bf30c6f3810143d75b
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-10-17 00:47:32 +0300

    added pokeball img

--- src/index.js
+++ src/index.js
@@ -15,32 +15,50 @@ const appendPokemonData = async ()=>{
     }
 }
 
-const appendTypeList = (list)=>{
-    for(li of list){
-        createTypeLiElement(li);
+const appendTypeList = (typeList)=>{
+    for(type of typeList){
+        const btn = createBtnElement(type);
+        btn.addEventListener('click',handlePokListEvent);
+        document.getElementById('types-list').appendChild(btn); 
     }
 }
 
-const createTypeLiElement = (li)=>{
+const appendPokemonByTypeList = (PokémonArray)=>{
+    for(Pokémon of PokémonArray){
+        const btn = createBtnElement(Pokémon);
+        btn.addEventListener('click',changeSearch);
+        document.getElementById('pokemon-list').appendChild(btn); 
+    }
+}
+
+const createBtnElement = (liTxt)=>{
     const btn = document.createElement('button');
     btn.setAttribute("class", "list-group-item list-group-item-action");  
     btn.setAttribute("type", "button");  
-    btn.innerText = li;         
-    btn.addEventListener('click',getPokémonsByType)
-    document.getElementById('types-list').appendChild(btn); 
+    btn.innerText = liTxt;         
+    return btn;
+}
+
+const changeSearch = ({target})=>{
+    clearDisplay();
+    document.getElementById('search-inpt').value=target.innerText;
+    appendPokemonData();
+    document.getElementById('data').style='display: block';
+    document.getElementById('pokemon-list-opener').style='display: none';
+
 }
 
 const getPokemonData = async ()=>{
-        const pokeId = document.getElementById('search-inpt').value;
-        const ApiResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`)
-        const pokeData = ApiResult.data;
-        return { name : pokeData.species.name,
-                 height : pokeData.height, 
-                 weight : pokeData.weight,
-                 image : pokeData.sprites.front_default,
-                 backImage : pokeData.sprites.back_default,
-                 types : pokeData.types
-                }
+    const searchTxt=document.getElementById('search-inpt').value;
+    const ApiResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTxt}/`)
+    const pokeData = ApiResult.data;
+    return { name : pokeData.species.name,
+             height : pokeData.height, 
+             weight : pokeData.weight,
+             image : pokeData.sprites.front_default,
+             backImage : pokeData.sprites.back_default,
+             types : pokeData.types
+            }
 }
 
 
@@ -58,7 +76,7 @@ const closeNotFoundMsg = ()=>{
     document.getElementById('not-found-msg').style='display: none';
 }
 
-const displayPokemonData = async ()=>{
+const displayPokemonData = async (e)=>{
     clearDisplay();
     if(await appendPokemonData())
     {
@@ -70,18 +88,22 @@ const clearDisplay =()=>{
     document.getElementById('not-found-msg').style='display: none';
     document.getElementById('data').style='display: none';
     document.getElementById('types-list').innerHTML = '';
+    document.getElementById('pokemon-list').innerHTML = '';
 };
 
-const getPokémonsByType = async ({target})=>{
+const getPokémonsByType = async (target)=>{
     const type = target.innerText;
     const ApiResult = await axios.get(`https://pokeapi.co/api/v2/type/${type}/`)
     const pokeData = ApiResult.data;
     const PokémonArray = pokeData.pokemon.map((pokemon)=>pokemon.pokemon.name)
-    console.log(PokémonArray)
-    // appendTypeList(PokémonArray)
     return PokémonArray;
 }
 
+const handlePokListEvent= async({target})=>{
+    document.getElementById('pokemon-list').innerHTML = '';
+    appendPokemonByTypeList(await getPokémonsByType(target));
+    document.getElementById('pokemon-list-opener').style='display: block';
+}
 
 
 
